bd6474dc09ddb598e463aa2d41f003b324e0852c
JascoSenac <joao.3931@aluno.pr.senac.br>
2025-04-30 16:47:27 -0300

    Create main.py
    fisrt commit

---
+++ main.py
@@ -0,0 +1,88 @@
+#Exercício 1.1: Olá, Mundo!
+# print("Bem-vindo ao mundo da programação!")
+
+
+#Exercício 2.1: Variáveis & Tipos de Dados
+# name = "João"    #string
+# age = 40         #int
+# height = 1.9     #float 
+# studing = True   #bool
+
+# print("Nome:", name, ", Idade:", age, ", Altura:", height, ", Estudando:", studing)
+
+# seleciona:
+# CTRL + ;
+# comentário em bloco
+
+#Exercício 2.2:
+# numero_um = 1
+# numero_dois = 2
+# numero_três = numero_um + numero_dois
+# print(numero_três)
+
+# Exercício 3.1: Entrada de Dados
+# nome = input("Nome: ")
+# idade = int(input("Idade: "))
+# print(f"Nome: {nome} | Idade: {idade}")
+
+# Exercício 3.2: Entrada de Dados
+# n1 = int(input("Número 1: "))
+# n2 = int(input("Número 2: "))
+
+# soma = n1 + n2
+# print(soma)
+
+# Exercício 4.1: Operadores Matemáticos
+# 1. ler um número
+# 2. exibir o DOBRO
+# 3. o TRIPLO
+# 4. RAIZ QUADRADA
+
+# # # # # # #
+#import math
+
+# num_user = int(input("Número: "))
+# double_num = (num_user * 2)
+# print(f"O dobro é: {double_num} ")
+# print("")
+
+# triple_num = (num_user * 3)
+# print(f"O dobro é: {triple_num} ")
+# print("")
+
+# sqrt_num = math.sqrt(num_user)
+# print(f"A raiz de {num_user} é:  {sqrt_num}")
+# print("")
+
+
+# num_user = int(input("Número: "))
+# sqrt_num = num_user ** 0.5
+# print(f"A raiz de {num_user} é:  {sqrt_num}")
+# print("")
+
+#4.2 Calcula IMC = peso / altura ** 2
+
+# peso = float(input("Infome o peso: "))
+# altura = float(input("Qual a altura: "))
+
+# imc = peso / altura ** 2
+# print(imc)
+
+#5.1 Condicionais
+# Peça a idade do usuário e classifique:
+# Criança (até 12 anos),
+# Adolescente (13 a 17),
+# Adulto (18 a 59),
+# Idoso (60 ou mais).
+
+# idade = int(input("Informe a idade: "))
+# if idade <= 12:
+#     print("Criança")
+# elif idade <= 17:
+#     print("Adolescente")
+# elif idade <= 59:
+#     print("Adulto")
+# else:
+#     print("Idoso")
+
+
